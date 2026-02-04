8827679a34557f07bf8513e425495e9684a84a44
Tanvi <257259061+tanvistack@users.noreply.github.com>
2026-02-04 19:53:29 +0530

    Add files via upload

---
+++ NumPy Statistics.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+#1.Generate 100 random numbers between 1 and 100
+
+data = np.random.randint(1,101,100)
+#here 100 is tot
+
+print("Random Numbers\n", data)
+
+#2.Calculate Statistics
+
+mean_value = np.mean(data)
+median_value = np.median(data)
+std_value = np.std(data)
+variance_value = np.var(data)
+
+#Standard Deviation
+#--------------------
+# Measures:how far numbers are from the mean
+# Small std → numbers are close
+# Large std → numbers are very spread
+#standard devaition is used for Real world interpretation 
+#-------------------------
+
+#Variance:Square of standard deviation,Shows mathematical spread of data
+#Variance = (Std Dev)²
+#variance is used for Mathematical Calculations
+
+
+
+
+print("\n📊 STATISTICS RESULT")
+print("Mean:", mean_value)
+print("Median:", median_value)
+print("Standard Deviation:", std_value)
+print("Variance:", variance_value)
